9be60b423f6829dd8bd1d614fe48e3500307a191
Leonid <erlygin.la@phystech.edu>
2018-10-05 11:53:42 +0300

    Babichev project first commit

---
+++ CMakeFiles/microsha.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/microsha.dir/main.cpp.o"
+  "microsha.pdb"
+  "microsha"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/microsha.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
